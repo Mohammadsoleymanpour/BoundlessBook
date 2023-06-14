a4c8f3303bae029e6aa886ba4f712b284955ba28
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-06-14 14:30:08 +0330

    Add Some Function For Order And Add Order Address And Add Last Update Field

--- BoundlessBook.Domain/OrderAggregate/Order.cs
+++ BoundlessBook.Domain/OrderAggregate/Order.cs
@@ -1,21 +1,70 @@
 ﻿using BoundlessBook.Common;
+using BoundlessBook.Common.Exceptions;
 using BoundlessBook.Domain.OrderAggregate.Enums;
 using BoundlessBook.Domain.OrderAggregate.ValueObjects;
 
 namespace BoundlessBook.Domain.OrderAggregate;
 
-public class Order:AggregateRoot
+public class Order : AggregateRoot
 {
+    public Order()
+    {
+
+    }
+    public Order(Guid userId)
+    {
+        UserId = userId;
+        OrderStatus = OrderStatus.Pending;
+    }
     public Guid UserId { get; set; }
     public OrderStatus OrderStatus { get; set; }
     public OrderDiscount? OrderDiscount { get; set; }
+    public OrderAddress Address { get; set; }
+    public DateTime LastUpdate { get; set; }
     public float TotalPrice => OrderItems.Sum(c => c.TotalPrice);
     public int ItemCount => OrderItems.Count();
-    
-    
+
     #region Relations
 
     public List<OrderItem> OrderItems { get; set; }
-    
+
     #endregion
+
+    public void AddItem(OrderItem item)
+    {
+        OrderItems.Add(item);
+    }
+
+    public void RemoveItem(Guid itemId)
+    {
+        var item = OrderItems.FirstOrDefault(c => c.Id == itemId);
+        if (item ==null)
+        {
+            throw new NullOrEmptyDomainException("آبتمی برای حذف یافت نشد");
+        }
+
+        OrderItems.Remove(item);
+    }
+
+    public void ChangeItemCount(Guid itemId, int count)
+    {
+        var item = OrderItems.FirstOrDefault(c => c.Id == itemId);
+        if (item == null)
+        {
+            throw new NullOrEmptyDomainException("آیتمی یافت نشد");
+        }
+
+        item.ChangeCount(count);
+    }
+
+    public void ChangeStatus(OrderStatus status)
+    {
+        OrderStatus = status;
+        LastUpdate = DateTime.Now;
+    }
+
+    public void CheckOutAddress(OrderAddress address)
+    {
+        Address = address;
+    }
 }
