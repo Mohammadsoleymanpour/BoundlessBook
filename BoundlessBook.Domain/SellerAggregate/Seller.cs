42f32f96514e67a482f594e47e4f9678529e04f1
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-06-20 09:01:36 +0330

    Implement Seller And Seller Inventory And Function Them

--- BoundlessBook.Domain/SellerAggregate/Seller.cs
+++ BoundlessBook.Domain/SellerAggregate/Seller.cs
@@ -1,15 +1,51 @@
 ﻿using BoundlessBook.Common;
+using BoundlessBook.Common.Exceptions;
 using BoundlessBook.Domain.SellerAggregate.Enums;
 
 namespace BoundlessBook.Domain.SellerAggregate;
 
 public class Seller:AggregateRoot
 {
+    public Seller(Guid userId, string shopName, string nationalCode)
+    {
+        Guard(shopName,nationalCode);
+        UserId = userId;
+        ShopName = shopName;
+        NationalCode = nationalCode;
+        
+
+    }
     public Guid UserId { get; set; }
 
     public string ShopName { get; set; }
     public string NationalCode { get; set; }
     public SellerStatus SellerStatus { get; set; }
+    public DateTime LastUpdate { get; set; }
     public List<SellerInventory> SellerInventories { get; set; }
+
+    public void ChangeStatus(SellerStatus status)
+    {
+        SellerStatus = status;
+        LastUpdate = DateTime.Now;
+    }
+
+    public void Edit(string name, string nationalCode)
+    {
+        Guard(name,nationalCode);
+        ShopName = name;
+        NationalCode = nationalCode;
+    }
+
+    public void Guard(string shopName,string nationalCode)
+    {
+        NullOrEmptyDomainException.CheckString(shopName,nameof(shopName));
+        NullOrEmptyDomainException.CheckString(nationalCode,nameof(nationalCode));
+        if (!IranianNationalCodeChecker.IsValid(nationalCode))
+        {
+            throw new InvalidDomainException("کد ملی وارد شده معتبر نیست");
+        }
+
+
+    }
 
 }
--- BoundlessBook.Domain/SellerAggregate/SellerInventory.cs
+++ BoundlessBook.Domain/SellerAggregate/SellerInventory.cs
@@ -1,6 +1,24 @@
-﻿namespace BoundlessBook.Domain.SellerAggregate;
+﻿using BoundlessBook.Common.Exceptions;
+
+namespace BoundlessBook.Domain.SellerAggregate;
 
 public class SellerInventory
 {
-    
+    public SellerInventory( Guid productId, int count, float price)
+    {
+        if (count<1)
+        {
+            throw new InvalidDomainException(" تعداد نمیتواند کمتر از یک باشد");
+        }
+        ProductId = productId;
+        Count = count;
+        Price = price;
+       
+    }
+    public Guid SellerId { get; set; }
+    public Guid ProductId { get; set; }
+    public int Count { get; set; }
+    public float Price { get; set; }
+    public bool IsActive { get; set; }
+
 }
