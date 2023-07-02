47e9f978f51fe9317da13a0645f46e8b815bab69
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-02 15:14:33 +0330

    Create Banner Entity

---
+++ BoundlessBook.Domain/SiteEntities/Banner.cs
@@ -0,0 +1,41 @@
+﻿using BoundlessBook.Common.Exceptions;
+
+namespace BoundlessBook.Domain.SiteEntities;
+
+public class Banner
+{
+    public Banner()
+    {
+        
+    }
+
+    public Banner(string link, string imageName, BannerPosition bannerPosition)
+    {
+        Guard(imageName,link);
+        Link = link;
+        ImageName = imageName;
+        BannerPosition = bannerPosition;
+    }
+    public string Link { get; set; }
+    public string ImageName { get; set; }
+    public BannerPosition BannerPosition { get; set; }
+
+    public void Edit(string link, string imageName, BannerPosition bannerPosition)
+    {
+        Guard(imageName,link);
+        Link = link;
+        ImageName = imageName;
+        BannerPosition = bannerPosition;
+    }
+
+    public void Guard(string imageName,string link)
+    {
+        NullOrEmptyDomainException.CheckString(imageName,nameof(imageName));
+        NullOrEmptyDomainException.CheckString(link,nameof(link));
+    }
+}
+
+public enum BannerPosition
+{
+    Right,Left
+}
