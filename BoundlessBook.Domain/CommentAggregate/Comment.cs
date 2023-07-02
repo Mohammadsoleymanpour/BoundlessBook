e676da1ccb089d3c4e3d93634d2877133c48d0e0
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-02 15:07:55 +0330

    Save Comment Class

--- BoundlessBook.Domain/CommentAggregate/Comment.cs
+++ BoundlessBook.Domain/CommentAggregate/Comment.cs
@@ -1,11 +1,39 @@
 ﻿using BoundlessBook.Common;
+using BoundlessBook.Common.Exceptions;
+using BoundlessBook.Domain.CommentAggregate.Enums;
 
 namespace BoundlessBook.Domain.CommentAggregate;
 
 public class Comment:AggregateRoot
 {
+    public Comment()
+    {
+        
+    }
+    public Comment(Guid userId, Guid productId, string text)
+    {
+        NullOrEmptyDomainException.CheckString(text,nameof(text));
+        UserId = userId;
+        ProductId = productId;
+        Text = text;
+        CommentStatus = CommentStatus.Pending;
+    }
     public Guid  UserId { get; set; }
     public Guid ProductId { get; set; }
     public string Text { get; set; }
-    
+    public CommentStatus CommentStatus { get; set; }
+    public DateTime LastUpdate { get; set; }
+
+    public void Edit(string text)
+    {
+        NullOrEmptyDomainException.CheckString(text, nameof(text));
+        Text =text;
+        LastUpdate=DateTime.Now;
+    }
+
+    public void ChangeStatus(CommentStatus status)
+    {
+        CommentStatus = status;
+        LastUpdate = DateTime.Now;
+    }
 }
--- BoundlessBook.Domain/CommentAggregate/Enums/CommentStatus.cs
+++ BoundlessBook.Domain/CommentAggregate/Enums/CommentStatus.cs
@@ -2,5 +2,5 @@
 
 public enum CommentStatus
 {
-    
+    Pending,Accept,Reject,
 }
