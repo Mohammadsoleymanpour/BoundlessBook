324d15d35317495f9c9bf027b01d93c494a9a2e1
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-04 11:17:57 +0330

    Add Base Repository For Aggregates

---
+++ BoundlessBook.Domain/CommentAggregate/ICommentRepository.cs
@@ -0,0 +1,8 @@
+﻿using BoundlessBook.Common.BaseRepository;
+
+namespace BoundlessBook.Domain.CommentAggregate;
+
+public interface ICommentRepository : IBaseRepository<Comment>
+{
+    
+}
---
+++ BoundlessBook.Domain/ProductAggregate/Repository/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using BoundlessBook.Common.BaseRepository;
+
+namespace BoundlessBook.Domain.ProductAggregate.Repository;
+
+public interface IProductRepository:IBaseRepository<Product>
+{
+    
+}
