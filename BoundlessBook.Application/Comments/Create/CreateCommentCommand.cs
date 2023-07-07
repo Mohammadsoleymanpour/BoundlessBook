e8221c03bf40388ee04dd607078374c76fa8062c
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-07 19:50:26 +0330

    Implement Create And Edit Comment

---
+++ BoundlessBook.Application/Comments/Create/CreateCommentCommand.cs
@@ -0,0 +1,5 @@
+﻿using BoundlessBook.Common.Common.Application;
+
+namespace BoundlessBook.Application.Comments.Create;
+
+public record CreateCommentCommand(Guid UserId, Guid ProductId, string Text) : IBaseCommand;
---
+++ BoundlessBook.Application/Comments/Create/CreateCommentValidator.cs
@@ -0,0 +1,17 @@
+﻿using BoundlessBook.Common.Common.Application.Validation;
+using FluentValidation;
+
+namespace BoundlessBook.Application.Comments.Create;
+
+public class CreateCommentValidator : AbstractValidator<CreateCommentCommand>
+{
+    public CreateCommentValidator()
+    {
+        RuleFor(c => c.ProductId).NotNull().NotEmpty();
+        RuleFor(c => c.ProductId).NotNull().NotEmpty();
+        RuleFor(c => c.Text).NotNull().NotEmpty()
+            .WithMessage(ValidationMessages.required("متن"))
+            .MaximumLength(500).WithMessage(ValidationMessages.maxLength("متن", 500))
+            .MinimumLength(5).WithMessage(ValidationMessages.minLength("متن", 5));
+    }
+}
