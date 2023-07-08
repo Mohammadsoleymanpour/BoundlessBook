093009193a18de657d550f06508772a3615c3b17
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-08 09:54:39 +0330

    Feat : Complete Comment Use Case

---
+++ BoundlessBook.Application/Comments/ChangeStatus/ChangeCommentStatusCommand.cs
@@ -0,0 +1,6 @@
+﻿using BoundlessBook.Common.Common.Application;
+using BoundlessBook.Domain.CommentAggregate.Enums;
+
+namespace BoundlessBook.Application.Comments.ChangeStatus;
+
+public record ChangeCommentStatusCommand(Guid CommentId, CommentStatus Status) : IBaseCommand;
