054750727ef195e15c9a141e186c74cea2a97eee
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-09 10:55:21 +0330

    Implement Checkout Order In Application And Implement Remove Order Item In Application

---
+++ BoundlessBook.Application/Orders/RemoveItem/RemoveOrderItemCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using BoundlessBook.Common.Common.Application.Validation;
+using FluentValidation;
+
+namespace BoundlessBook.Application.Orders.RemoveItem;
+
+public class RemoveOrderItemCommandValidator:AbstractValidator<RemoveOrderItemCommand>
+{
+    public RemoveOrderItemCommandValidator()
+    {
+        RuleFor(c => c.ItemId).NotNull().NotEmpty().WithMessage(ValidationMessages.required("آیدی ایتم"));
+        RuleFor(c => c.UserId).NotNull().NotEmpty().WithMessage(ValidationMessages.required("آیدی کاربر"));
+
+    }
+}
