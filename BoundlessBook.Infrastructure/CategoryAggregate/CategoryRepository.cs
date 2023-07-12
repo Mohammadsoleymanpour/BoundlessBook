e8d88ae9466c9713fea02b5381a739341a3c0b9b
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-12 16:18:32 +0330

    Implement Product And Category Repository

---
+++ BoundlessBook.Infrastructure/CategoryAggregate/CategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using BoundlessBook.Domain.CategoryAggregate;
+using BoundlessBook.Infrastructure._Utilities;
+
+namespace BoundlessBook.Infrastructure.CategoryAggregate;
+
+public class CategoryRepository:BaseRepository<Category>, ICategoryRepository
+{
+    public CategoryRepository(BoundlessBookContext context) : base(context)
+    {
+    }
+}
--- BoundlessBook.Infrastructure/ProductAggregate/ProductRepository.cs
+++ BoundlessBook.Infrastructure/ProductAggregate/ProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace BoundlessBook.Infrastructure.ProductAggregate;
+﻿using BoundlessBook.Domain.ProductAggregate;
+using BoundlessBook.Domain.ProductAggregate.Repository;
+using BoundlessBook.Infrastructure._Utilities;
 
-public class ProductRepository
+namespace BoundlessBook.Infrastructure.ProductAggregate;
+
+public class ProductRepository:BaseRepository<Product>, IProductRepository
 {
-    
+    public ProductRepository(BoundlessBookContext context) : base(context)
+    {
+    }
 }
