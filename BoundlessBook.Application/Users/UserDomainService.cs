e7823d78d1ce31d5cdc3d2590d12b243bf38f2b9
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-18 15:26:56 +0330

    Add Config Layer And Create Domain Service Classes

---
+++ BoundlessBook.Application/Users/UserDomainService.cs
@@ -0,0 +1,16 @@
+﻿using BoundlessBook.Domain.UserAggregate.Services;
+
+namespace BoundlessBook.Application.Users;
+
+public class UserDomainService : IUserDomainService
+{
+    public bool IsPhoneNumberExist(string phoneNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsEmailExist(string email)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BoundlessBook.Config/BoundlessBookConfig.cs
@@ -0,0 +1,34 @@
+﻿using BoundlessBook.Application.Categories;
+using BoundlessBook.Application.Products;
+using BoundlessBook.Application.Sellers;
+using BoundlessBook.Application.Users;
+using BoundlessBook.Common.Common.Application.Utilities;
+using BoundlessBook.Domain.CategoryAggregate.Service;
+using BoundlessBook.Domain.ProductAggregate.Services;
+using BoundlessBook.Domain.SellerAggregate.Services;
+using BoundlessBook.Domain.UserAggregate.Services;
+using BoundlessBook.Infrastructure;
+using BoundlessBook.Query.Categories.GetById;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BoundlessBook.Config
+{
+    public static class BoundlessBookConfig
+    {
+
+        public static void RegisterDependency(IServiceCollection service, string connectionString)
+        {
+            InfrastructureDI.Init(service,connectionString);
+
+            service.AddMediatR(typeof(Directories).Assembly);
+
+            service.AddMediatR(typeof(GetCategoryByIdQuery).Assembly);
+
+            service.AddTransient<IProductDomainService, ProductDomainService>();
+            service.AddTransient<ICategoryDomainService, CategoryDomainService>();
+            service.AddTransient<IUserDomainService, UserDomainService>();
+            service.AddTransient<ISellerDomainService, SellerDomainService>();
+        }
+    }
+}
