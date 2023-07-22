0c7264391bc38d5e2f06a466c7e30bf828bbfd79
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-22 11:04:22 +0330

    Implement Get Product By Slug

---
+++ BoundlessBook.Query/Products/GetBySlug/GetProductBySlugQuery.cs
@@ -0,0 +1,6 @@
+﻿using BoundlessBook.Common.Common.Query;
+using BoundlessBook.Query.Products.DTOs;
+
+namespace BoundlessBook.Query.Products.GetBySlug;
+
+public record GetProductBySlugQuery(string Slug) : IQuery<ProductDto>;
---
+++ BoundlessBook.Query/Products/GetBySlug/GetProductBySlugQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using BoundlessBook.Common.Common.Query;
+using BoundlessBook.Infrastructure.Ef.Persistent;
+using BoundlessBook.Query.Products.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoundlessBook.Query.Products.GetBySlug;
+
+public class GetProductBySlugQueryHandler : IQueryHandler<GetProductBySlugQuery, ProductDto>
+{
+    private readonly BoundlessBookContext _context;
+
+    public GetProductBySlugQueryHandler(BoundlessBookContext context)
+    {
+        _context = context;
+    }
+    public async Task<ProductDto> Handle(GetProductBySlugQuery request, CancellationToken cancellationToken)
+    {
+        var product = await _context.Products.FirstOrDefaultAsync(c => c.Slug == request.Slug, cancellationToken);
+        if (product == null)
+        {
+            return null;
+        }
+
+        var model = product.Map();
+        model = await model.SetCategories(_context);
+
+        return model;
+    }
+}
