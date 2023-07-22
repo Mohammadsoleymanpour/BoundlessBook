f8c2096b51ac48364398b7ada39d3694dd02a2cc
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-22 10:16:07 +0330

    Save File

--- BoundlessBook.Query/Products/DTOs/ProductDto.cs
+++ BoundlessBook.Query/Products/DTOs/ProductDto.cs
@@ -10,7 +10,7 @@ public class ProductDto:BaseDto
     public string Title { get; set; }
     public string Description { get; set; }
     public string ImageName { get; set; }
-    public Guid CategoryId { get; set; }
+    public Guid CategoryId { get; set; } 
     public Guid SubCategoryId { get; set; }
     public Guid SecondarySubCategoryId { get; set; }
     public string Slug { get; set; }
