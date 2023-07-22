b1b86a85350b9b7c6df14472fbc43eb787adf1ca
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-22 11:19:11 +0330

    Implement Get Product By Filter

---
+++ BoundlessBook.Query/Products/GetByFilter/GetProductByFilterQuery.cs
@@ -0,0 +1,11 @@
+﻿using BoundlessBook.Common.Common.Query;
+using BoundlessBook.Query.Products.DTOs;
+
+namespace BoundlessBook.Query.Products.GetByFilter;
+
+public class GetProductByFilterQuery:QueryFilter<ProductFilterResult,ProductFilterParams>
+{
+    public GetProductByFilterQuery(ProductFilterParams param) : base(param)
+    {
+    }
+}
---
+++ BoundlessBook.Query/Products/GetByFilter/GetProductByFilterQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using BoundlessBook.Common.Common.Query;
+using BoundlessBook.Infrastructure.Ef.Persistent;
+using BoundlessBook.Query.Products.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoundlessBook.Query.Products.GetByFilter;
+
+public class GetProductByFilterQueryHandler : IQueryHandler<GetProductByFilterQuery, ProductFilterResult>
+{
+    private readonly BoundlessBookContext _context;
+
+    public GetProductByFilterQueryHandler(BoundlessBookContext context)
+    {
+        _context = context;
+    }
+    public async Task<ProductFilterResult> Handle(GetProductByFilterQuery request, CancellationToken cancellationToken)
+    {
+        var @params = request.FilterParam;
+
+        var result = _context.Products.OrderByDescending(c => c.CreationDate).AsQueryable();
+
+        if (@params.Id != null)
+        {
+            result = result.Where(c => c.Id == @params.Id);
+        }
+
+        if (string.IsNullOrEmpty(@params.Slug) is false)
+        {
+            result = result.Where(c => c.Slug == @params.Slug);
+        }
+
+        if (string.IsNullOrEmpty(@params.Title) is false)
+        {
+            result = result.Where(c => c.Title.Contains(@params.Title));
+        }
+
+        var skip = (@params.PageId - 1) * @params.Take;
+
+        var model = new ProductFilterResult()
+        {
+            FilterParams = @params,
+            Data = await result.Skip(skip).Take(@params.Take).Select(c => c.MapListData())
+                .ToListAsync(cancellationToken)
+        };
+
+        model.GeneratePaging(result, @params.Take, @params.PageId);
+        return model;
+    }
+}
