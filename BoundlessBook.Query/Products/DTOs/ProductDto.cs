09e552dde716401f8c24b4c4b4043aec958a579e
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-22 10:16:01 +0330

    Create Product Dtos

---
+++ BoundlessBook.Query/Products/DTOs/ProductDto.cs
@@ -0,0 +1,52 @@
+﻿using BoundlessBook.Common.Common.Domain.ValueObjects;
+using BoundlessBook.Common.Common.Query;
+using BoundlessBook.Common.Common.Query.Filter;
+using BoundlessBook.Domain.ProductAggregate;
+
+namespace BoundlessBook.Query.Products.DTOs;
+
+public class ProductDto:BaseDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageName { get; set; }
+    public Guid CategoryId { get; set; }
+    public Guid SubCategoryId { get; set; }
+    public Guid SecondarySubCategoryId { get; set; }
+    public string Slug { get; set; }
+    public SeoData SeoData { get; set; }
+    public List<ProductImageDto> ProductImages { get; set; }
+    public List<ProductSpecificationDto> ProductSpecifications { get; set; }
+}
+
+public class ProductFilterData : BaseDto
+{
+    public string Title { get; set; }
+    public string ImageName { get; set; }
+    public string Slug { get; set; }
+}
+
+public class ProductFilterResult : BaseFilter<ProductFilterData, ProductFilterParams>
+{
+
+}
+
+public class ProductFilterParams : BaseFilterParam
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Slug { get; set; }
+}
+
+public class ProductImageDto : BaseDto
+{
+    public Guid ProductId { get; set; }
+    public string ImageName { get; set; }
+    public int Sequence { get; set; }
+}
+
+public class ProductSpecificationDto : BaseDto
+{
+    public string Key { get; set; }
+    public string Value { get; set; } 
+}
