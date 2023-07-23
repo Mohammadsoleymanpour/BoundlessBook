3aba54ed521cfbcd8e44aa8a3658c5b301eb5431
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-07-23 11:03:49 +0330

    Create FacadDI Class And Implement CategoryFacade

---
+++ BoundlessBook.Presentation.Facade/FacadeDI.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BoundlessBook.Presentation.Facade;
+
+public static class FacadeDI
+{
+    public static void Init(this IServiceCollection services)
+    {
+
+    }
+}
