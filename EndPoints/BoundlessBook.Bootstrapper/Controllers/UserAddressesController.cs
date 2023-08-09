fae2263871f7f45bb13ebee52da651e36787492e
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-08-09 11:05:02 +0330

    Create UserAddress Controller

---
+++ EndPoints/BoundlessBook.Bootstrapper/Controllers/UserAddressesController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using BoundlessBook.Application.Users.AddAddress;
+using BoundlessBook.Application.Users.DeleteAddress;
+using BoundlessBook.Application.Users.EditAddress;
+using BoundlessBook.Bootstrapper.ViewModels.UserAddress;
+using BoundlessBook.Common.Common.Application;
+using BoundlessBook.Common.Common.AspNetCore;
+using BoundlessBook.Presentation.Facade.Users.UserAddress;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoundlessBook.Bootstrapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAddressesController : ControllerBase
+    {
+        private readonly IUserAddressFacade _userAddressFacade;
+        private readonly IMapper _mapper;
+
+        public UserAddressesController(IUserAddressFacade userAddressFacade, IMapper mapper)
+        {
+            _userAddressFacade = userAddressFacade;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<OperationResult> AddAddress(AddUserAddressViewModel viewModel)
+        {
+            var command = _mapper.Map<AddAddressCommand>(viewModel);
+            command.UserId = User.GetUserId();
+            return await _userAddressFacade.AddAddress(command);
+        }
+
+        [HttpPut]
+        public async Task<OperationResult> EditAddress(EditAddressCommand command)
+        {
+            return await _userAddressFacade.EditAddress(command);
+        }
+
+        [HttpDelete]
+        public async Task<OperationResult> DeleteAddress(Guid addressId)
+        {
+            var command = new DeleteAddressCommand(new Guid(), addressId);
+            return await _userAddressFacade.DeleteAddress(command);
+        }
+    }
+}
---
+++ EndPoints/BoundlessBook.Bootstrapper/Infrastructure/MapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BoundlessBook.Application.Users.AddAddress;
+using BoundlessBook.Bootstrapper.ViewModels.UserAddress;
+
+namespace BoundlessBook.Bootstrapper.Infrastructure;
+
+public class MapperProfile:Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<AddAddressCommand, AddUserAddressViewModel>().ReverseMap();
+    }
+}
