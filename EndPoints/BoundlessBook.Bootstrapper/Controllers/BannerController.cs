39186442d6e75d10efa8c8ce5b73a4569b8fc837
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-08-09 10:10:44 +0330

    Create Banner Controller

---
+++ EndPoints/BoundlessBook.Bootstrapper/Controllers/BannerController.cs
@@ -0,0 +1,46 @@
+﻿using BoundlessBook.Application.SiteEntities.Banner.Create;
+using BoundlessBook.Application.SiteEntities.Banner.Edit;
+using BoundlessBook.Common.Common.Application;
+using BoundlessBook.Presentation.Facade.SiteEntities.Banners;
+using BoundlessBook.Query.SiteEntities.Banners.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoundlessBook.Bootstrapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannerController : ControllerBase
+    {
+        private readonly IBannerFacade _bannerFacade;
+
+        public BannerController(IBannerFacade bannerFacade)
+        {
+            _bannerFacade = bannerFacade;
+        }
+
+        [HttpGet]
+        public async Task<List<BannerDto>> GetBanners()
+        {
+            return await _bannerFacade.GetBannerList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<BannerDto> GetBannerById(Guid id)
+        {
+            return await _bannerFacade.GetBannerById(id);
+        }
+
+        [HttpPost]
+        public async Task<OperationResult> CreateBanner(CreateBannerCommand command)
+        {
+            return await _bannerFacade.Create(command);
+        }
+
+        [HttpPut]
+        public async Task<OperationResult> EditBanner(EditBannerCommand command)
+        {
+            return await _bannerFacade.Edit(command);
+        }
+    }
+}
