1eb4c3bfe720df8084e826e117d6692aedcacc7b
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-09-10 11:10:31 +0330

    Implement Save Token In Command And CommandHandler And Facad Layer

---
+++ BoundlessBook.Application/Users/AddToken/AddUserTokenCommand.cs
@@ -0,0 +1,22 @@
+﻿using BoundlessBook.Common.Common.Application;
+
+namespace BoundlessBook.Application.Users.AddToken;
+
+public class AddUserTokenCommand : IBaseCommand
+{
+    public AddUserTokenCommand(Guid userId, string hashJwtToken, string hashRefreshToken, DateTime tokenExpireDate, DateTime refreshTokenExpireDate, string device)
+    {
+        UserId = userId;
+        HashJwtToken = hashJwtToken;
+        HashRefreshToken = hashRefreshToken;
+        TokenExpireDate = tokenExpireDate;
+        RefreshTokenExpireDate = refreshTokenExpireDate;
+        Device = device;
+    }
+    public Guid UserId { get; set; }
+    public string HashJwtToken { get; private set; }
+    public string HashRefreshToken { get; private set; }
+    public DateTime TokenExpireDate { get; private set; }
+    public DateTime RefreshTokenExpireDate { get; private set; }
+    public string Device { get; private set; }
+}
