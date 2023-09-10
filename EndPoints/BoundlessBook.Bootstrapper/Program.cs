65a3c6b48ce454b64c19a65ec808dde049585883
m.soleimanpoor <Mohammad.h.spour@gmail.com>
2023-09-10 08:14:25 +0330

    Auth middleware

--- EndPoints/BoundlessBook.Bootstrapper/Program.cs
+++ EndPoints/BoundlessBook.Bootstrapper/Program.cs
@@ -36,7 +36,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 app.UseApiCustomExceptionHandler();
 app.MapControllers();
